9f1a56f3be487eb0be13ca76aafa3f702d763bdc
kalelfleith <kalelfleith03@gmail.com>
2022-01-10 21:56:28 -0300

    Validação dos dados do formulário
    - Utilizando express-validator para validar campos
    - Exibindo erros de validação
    - Mantendo o preenchimento dos campos após validação

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,24 @@
 module.exports = function (app) {
     app.get('/formulario_inclusao_noticia', function(req, res){
-        res.render("admin/form_add_noticia");
+        res.render("admin/form_add_noticia", {validacao :{}, noticia:{}});
     });
 
     app.post('/noticias/salvar', function(req, res){
         //Informações via post o express popula o body com request;
         var noticia = req.body;
+
+        req.assert('title', 'Campo Titulo é obrigatório').notEmpty();
+        req.assert('resumo', 'Campo Resumo deve conter entre 10 e 100 caracteres').len(10, 100);     
+        req.assert('autor', 'Campo Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Campo Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('notice', 'Campo Noticia é obrigatória').notEmpty();
+        
+        var erros = req.validationErrors();
+
+        if (erros) {
+            res.render("admin/form_add_noticia", {validacao : erros, noticia : noticia});
+            return;
+        }
 
         var connection = app.config.dbConnection();
         var noticiasModel = new app.app.models.NoticiasDAO(connection);
